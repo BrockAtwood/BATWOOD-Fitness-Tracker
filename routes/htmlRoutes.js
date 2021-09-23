0bf20f6e2f008064d06261233c3af8791a38388a
Brock Atwood <brock.atwood@du.edu>
2021-09-23 00:28:42 -0600

    fixing bug in routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,18 +6,22 @@ const router = require("express").Router();
 
 //exercise.html
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
 });
 
 //index.html
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 //stats.html
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
+  res.sendFile(path.join(__dirname, "public/stats.html"));
 });
+
+//public/stats.html
+//public/index.html
+//public/exercise.html
 
 //exporting module
 module.exports = router;
