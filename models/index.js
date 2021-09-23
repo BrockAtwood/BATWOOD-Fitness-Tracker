f4c9abd32422afcd10a27a71c729c0538ea562ba
Brock Atwood <brock.atwood@du.edu>
2021-09-22 21:40:39 -0600

    setting up User story items

---
+++ models/index.js
@@ -0,0 +1,6 @@
+//Activity #15
+//exporting an object containing all of our models
+
+model.exports = {
+  Workout: require("./workout"),
+};
---
+++ models/workout.js
@@ -0,0 +1,55 @@
+//Activity #13 & #15
+
+//requiring mongoose and schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//User Story: from directions
+//Given Day
+//Schema tracking - name, type, weight, sets, reps, and duration of exercise.
+//If cardio exercise -- track distance traveled
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercise: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Activity name is required",
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: "Activity duration is required",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+  totalDuration: {
+    type: Number,
+  },
+});
+
+//Mongoose model
+const Workout = mongoose.model("Workout", workoutSchema);
+
+//model export
+module.exports = Workout;
